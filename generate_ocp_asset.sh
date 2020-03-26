a6a659a8e20597d6f313cccbf32ee61dd0afd633
karmab <karimboumedhel@gmail.com>
2020-03-26 11:47:30 +0100

    initial commit

---
+++ generate_ocp_asset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE64="gbase64"
+CONFIG=$(cat ~/.kcli/config.yml | $BASE64 -w 0)
+ID_RSA_PUB=$(cat ~/.kcli/id_rsa.pub | $BASE64 -w 0)
+ID_RSA=$(cat ~/.kcli/id_rsa | $BASE64 -w 0)
+oc create secret generic kcli-config-yml --from-file=filename=config.yml
+oc annotate secret/kcli-config-yml jenkins.openshift.io/secret.name=kcli-config-yml
+oc label secret/kcli-config-yml credential.sync.jenkins.openshift.io=true
+
+oc create secret generic kcli-id-rsa --from-file=filename=~/.ssh/id_rsa
+oc annotate secret/kcli-id-rsa jenkins.openshift.io/secret.name=kcli-id-rsa
+oc label secret/kcli-id-rsa credential.sync.jenkins.openshift.io=true
+
+oc create secret generic kcli-id-rsa-pub --from-file=filename=$HOME/.ssh/id_rsa.pub
+oc annotate secret/kcli-id-rsa-pub jenkins.openshift.io/secret.name=kcli-id-rsa-pub
+oc label secret/kcli-id-rsa-pub credential.sync.jenkins.openshift.io=true
